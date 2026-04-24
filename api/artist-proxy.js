a0f60d09780e8cd1e2ab6d955480326ac3e8bce9
4blockrao <4praveenrao@gmail.com>
2026-04-24 15:35:23 +0530

    Delete api/artist-proxy.js

--- api/artist-proxy.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  const { slug } = req.query;
-  if (!slug) return res.status(400).send('Missing slug');
-  const upstream = `https://rbenryjgtbrjvqvxbigq.supabase.co/functions/v1/artist-ssr?slug=${encodeURIComponent(slug)}`;
-  const upstreamRes = await fetch(upstream);
-  const html = await upstreamRes.text();
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  res.status(200).send(html);
-}
